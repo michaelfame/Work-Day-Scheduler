a23d5bc659e403d067fe429306e17ee8b0e7ac09
michaelfame <michaelfame@gmail.com>
2023-09-12 13:09:50 +0100

    added the js file to the html, added the timed notification and show feature to the JS and the CSS docs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
+$(document).ready(function(){
 
+    // the save button click to listen
+
+    $(".saveButton").on("click", function(){
+        console.log("testing the saved listener");
+
+        let timeID = $(this).parent(). attr("id")
+        let value = $(this).siblings(".description").val();
+
+        console.log(timeID);
+        console.log($(value));
+
+        localStorage.setItem(timeID, value);
+
+        // function to set notification for events 
+        $(".notification").addClass("show");
+
+        // function to remove notification for events 
+        setTimeout(function() {
+        $(".notification").removeClass("show");
+        
+        },5000)
+
+    })
+
+
+
+
+
+
+
+
+
+
+})

57ddd08790b435cd5ce9a19a98665e16e80e8567
michaelfame <michaelfame@gmail.com>
2023-09-12 23:46:46 +0100

    added the hourly codes and worked on the readme file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Work-Day-Scheduler
 To create a Daily planner to manage work schedules
+
+
+It allows a user to save events for each hour of the daye. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by jQuery
+
+## Github repo details: 
+
+## Url details can be found : 
+
+### AIM of the Assignment
+Using knowledge gained in JQuery,Day.js, CSS and HTML,
+
+The Web App is expected to Display the current day at the top of the calender when a user opens the planner.
+ 
+* Present timeblocks for standard business hours when the user scrolls down.
+ 
+* Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+ 
+* Allow a user to enter an event when they click a timeblock
+
+* Save the event in local storage when the save button is clicked in that timeblock.
+  
+  ### Challenges:
+Learning to work with newJQuery methods, Day.js formats, developing functions to allow time blocks to be formed and iterated over, etc.
+
+ Licenses used was by MIT for free open source reuse as found in the Repository found in github account.
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     // the save button click to listen
 
     $(".saveButton").on("click", function(){
-        console.log("testing the saved listener");
+        console.log("Event Saved");
 
         let timeID = $(this).parent(). attr("id")
         let value = $(this).siblings(".description").val();
@@ -23,6 +23,70 @@ $(document).ready(function(){
         },5000)
 
     })
+ // function to create the time block hourly update
+
+ 
+    function hourUpdate(){
+        let currentHour = moment().hours();
+      
+        for(let i = 0; i < $(".time-block").length; i++){
+            let hour = parseInt($(".time-block")[1].getAttribute("id").split("-")[1])}
+            console.log(hour);
+            console.log(currentHour);
+
+            if (hour < currentHour) {
+                $(".time-block")[1].classList.add("past")
+            } else if(hour === currentHour){
+                $(".time-block")[1].classList.add("past")
+                $(".time-block")[1].classList.remove("present")
+            } else {
+                $(".time-block")[1].classList.add("past")
+                $(".time-block")[1].classList.remove("present")                
+                $(".time-block")[1].classList.add("future")
+            }
+            }
+            
+
+ 
+            hourUpdate();
+
+            let intervalv = setInterval(hourUpdate, 15000);
+            
+            $("#hour-0 .description").val(localStorage.getItem("#hour-0"))
+            $("#hour-1 .description").val(localStorage.getItem("#hour-1"))
+            $("#hour-2 .description").val(localStorage.getItem("#hour-2"))
+            $("#hour-3 .description").val(localStorage.getItem("#hour-3"))
+            $("#hour-4 .description").val(localStorage.getItem("#hour-4"))
+            $("#hour-5 .description").val(localStorage.getItem("#hour-5"))
+            $("#hour-6 .description").val(localStorage.getItem("#hour-6"))
+            $("#hour-7 .description").val(localStorage.getItem("#hour-7"))
+            $("#hour-8 .description").val(localStorage.getItem("#hour-8"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-9"))
+            $("#hour-10 .description").val(localStorage.getItem("#hour-10"))
+            $("#hour-11 .description").val(localStorage.getItem("#hour-11"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-12"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-13"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-14"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-15"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-16"))
+            $("#hour-9 .description").val(localStorage.getItem("#hour-17"))
+            
+            $("#currentDay").text(moment().format("dddd, MMMM, Do"))
+
+        })
+
+
+
+        
+        
+        
+        
+        
+        
+
+         
+
+    
 
 
 
@@ -33,4 +97,3 @@ $(document).ready(function(){
 
 
 
-})
